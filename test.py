2db018dd0fe1d2c498a2be4e810828893c49543b
gbwgithub <78434849@qq.com>
2016-01-13 16:29:05 +0800

    主模块执行流程以函数划分

--- test.py
+++ test.py
@@ -1,10 +1,77 @@
 #coding=utf-8
 
+#-------------------------------------------------------------------------
+# 测试 import shutil
+#-------------------------------------------------------------------------
+import shutil
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------
+# 测试 os.path.exists()
+#-------------------------------------------------------------------------
+# import os
+# TE_DEMO_PATH = "D:\Android\eclipse-adt\workspace\TEDemo"
+# print os.path.exists(TE_DEMO_PATH)
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------
+# 测试 help(os)
+#-------------------------------------------------------------------------
+# import os
+# print dir(os)
+# print help(os)
+
+
+
+
+
+
+
 
 #-------------------------------------------------------------------------
 # 测试 git push --set-upstream origin master 后能否直接 git push
 #-------------------------------------------------------------------------
+# 结果是可以的，远程git仓库的内容有更新
+# $ git push
+# warning: push.default is unset; its implicit value has changed in
+# Git 2.0 from 'matching' to 'simple'. To squelch this message
+# and maintain the traditional behavior, use:
 
+#   git config --global push.default matching
+
+# To squelch this message and adopt the new behavior now, use:
+
+#   git config --global push.default simple
+
+# When push.default is set to 'matching', git will push local branches
+# to the remote branches that already exist with the same name.
+
+# Since Git 2.0, Git defaults to the more conservative 'simple'
+# behavior, which only pushes the current branch to the corresponding
+# remote branch that 'git pull' uses to update the current branch.
+
+# See 'git help config' and search for 'push.default' for further information.
+# (the 'simple' mode was introduced in Git 1.7.11. Use the similar mode
+# 'current' instead of 'simple' if you sometimes use older versions of Git)
+
+# Counting objects: 3, done.
+# Delta compression using up to 4 threads.
+# Compressing objects: 100% (3/3), done.
+# Writing objects: 100% (3/3), 402 bytes | 0 bytes/s, done.
+# Total 3 (delta 2), reused 0 (delta 0)
+# To git@github.com:gbwgithub/g-py.git
+#    0f26825..66cd4a7  master -> master
 
 
 
